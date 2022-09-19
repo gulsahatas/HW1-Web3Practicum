0f5e167d3007509164928bc77bee7e6661b2df0b
gulsahatas <101052413+gulsahatas@users.noreply.github.com>
2022-09-19 11:29:00 +0300

    Update FeeCollector.sol

--- FeeCollector.sol
+++ FeeCollector.sol
@@ -1,22 +1,23 @@
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.7; //specify version
 // SPDX-License-Identifier: MIT
 
 contract FeeCollector {
-    address public owner;
+    address public owner; // 
     uint256 public balance;
     constructor () {
-        owner = msg.sender;
+        owner = msg.sender; // owner is the deployer of the contract
 
     }
     receive() payable external {
-        balance += msg.value;
+        balance += msg.value; //balance is increased by msg.value whenever someone sends money to the contract
     }
 
-    function withdraw(uint amount, address payable destAddr) public {
-        require(msg.sender == owner, "Only owner can withdraw");
-        require(amount <= balance, "Insufficient funds");
-        destAddr.transfer(amount);
-        balance -= amount;
+    function withdraw(uint amount, address payable destAddr) public { // this function extracts money from "destAddr" by "amount"
+    
+        require(msg.sender == owner, "Only owner can withdraw");  //since the withdraw function is public, add a level of security by checking if the function is called by the owner
+        require(amount <= balance, "Insufficient funds"); // check if there's enough money on the account to withdraw the specified amount
+        destAddr.transfer(amount); //if sonditions are met, transfer "amount" to destAddr
+        balance -= amount; //decrease the balance by the withdrawn amount
     }
 
 }
